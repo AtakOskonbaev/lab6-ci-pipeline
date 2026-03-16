28827629c0ce7d46977d939e430e3cafeab2452d
AtakOskonbaev <atakoskonbaev21@gmail.com>
2026-03-16 11:27:30 +0600

    fixes2

--- src/calculator.py
+++ src/calculator.py
@@ -4,11 +4,14 @@
 def add(a: float, b: float) -> float:
     return a + b
 
+
 def subtract(a: float, b: float) -> float:
     return a - b
 
+
 def multiply(a: float, b: float) -> float:
     return a * b
+
 
 def divide(a: float, b: float) -> float:
     if b == 0:
