c0bc52a4cd507e6729abf82ad0b2173bf437ec6a
AtakOskonbaev <atakoskonbaev21@gmail.com>
2026-03-16 11:25:07 +0600

    fixes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,23 +2,23 @@ import pytest
 from src.calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2,3) == 5
-    assert add(-1,1) == 0
-    assert add(0,0) == 0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
 def test_subtract():
-    assert subtract(5,3) == 2
-    assert subtract(0,5) == -5
+    assert subtract(5, 3) == 2
+    assert subtract(0, 5) == -5
 
 def test_multiply():
-    assert multiply(2,3) == 6
-    assert multiply(-2,3) == -6
-    assert multiply(0,100) == 0
+    assert multiply(2, 3) == 6
+    assert multiply(-2, 3) == -6
+    assert multiply(0, 100) == 0
 
 def test_divide():
-    assert divide(6,2) == 3
-    assert divide(5,2) == 2.5
+    assert divide(6, 2) == 3
+    assert divide(5, 2) == 2.5
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
-        divide(5,0)
+        divide(5, 0)
